bd9a377e496455b33de8e4f8f696b7223527617d
liugang <liugang@cnezsoft.com>
2017-02-23 16:30:11 +0800

    * Check if the module or method in request if illegal. * Adjust daemon.sh.

--- server/app/xuanxuan/daemon.sh
+++ server/app/xuanxuan/daemon.sh
@@ -5,7 +5,7 @@
 # @author Gang Liu <liugang@cnezsoft.com>
 #
 username='www-data'  # Use the run user of your web server as username.
-running=`ps aux|grep 'php ./server.php'|awk '$1=="www-data"{print $0}'`
+running=`ps aux|grep 'php ./server.php'|grep -v 'grep'`
 if [ ! "$running" ]; then
     nohup sudo sudo -u $username ./server.php 2>1&
 fi
