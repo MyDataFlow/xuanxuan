fc8c98622b494b78b523e0629ba3884e84f4dd62
liugang <liugang@cnezsoft.com>
2017-02-23 11:00:27 +0800

    * Remove useless lang files.

--- server/app/xuanxuan/server.php
+++ server/app/xuanxuan/server.php
@@ -13,13 +13,10 @@ include $serverRoot . '/../../framework/model.class.php';
 include $serverRoot . '/../../framework/helper.class.php';
 include $serverRoot . '/daemon.class.php';
 
-$_COOKIE['lang'] = 'zh-cn';
 /* Instance the daemon and run it. */
 $app = daemon::createApp('xuanxuan', '', 'daemon');
 $common = $app->loadCommon();
 $port   = $app->config->socket->port;
 if(!$port) $port = '8080';
-
-echo sprintf($app->lang->xuanxuan->start, __FILE__);
 
 $app->start('0.0.0.0', $port);
