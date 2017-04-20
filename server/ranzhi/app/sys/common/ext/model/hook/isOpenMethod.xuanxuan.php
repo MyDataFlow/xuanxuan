7a4a04ed5e03b5c0ce0e6ba2c2ed1e90c94f3503
liugang <liugang@cnezsoft.com>
2017-04-20 17:53:01 +0800

    * Add entrance for xuanxuan. * Change api.

--- server/ranzhi/app/sys/common/ext/model/hook/isOpenMethod.xuanxuan.php
+++ server/ranzhi/app/sys/common/ext/model/hook/isOpenMethod.xuanxuan.php
@@ -1,3 +1,5 @@
 <?php
+if($module == 'api' and $method == 'xuanxuan') return true;
+if($module == 'chat') return true;
 if($module == 'attach' and $method == 'upload')   return true;
 if($module == 'attach' and $method == 'download') return true;
