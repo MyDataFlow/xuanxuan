b74d6259360ccbe247d6cce1e70c06baa80f484d
Catouse <catouse@gmail.com>
2017-04-27 10:30:19 +0800

    * disabled flash tray on window focus.

--- app/app.js
+++ app/app.js
@@ -184,6 +184,7 @@ class App extends ReadyNotifier {
 
         this.browserWindow.on('focus', () => {
             this.emit(R.event.ui_focus_main_window);
+            this.flashTrayIcon(false);
         });
 
         this.browserWindow.on('blur', () => {
