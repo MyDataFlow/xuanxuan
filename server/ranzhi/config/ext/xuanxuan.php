120cc2e0e330ec459f3822f74374be612a6e634b
liugang <liugang@cnezsoft.com>
2017-04-28 15:27:38 +0800

    * Adjust default key.

--- server/ranzhi/config/ext/xuanxuan.php
+++ server/ranzhi/config/ext/xuanxuan.php
@@ -1,7 +1,7 @@
 <?php
 $config->xuanxuan = new stdclass();
 $config->xuanxuan->version = '1.1.0';
-$config->xuanxuan->key     = 'tokenID8888888888888888888888888';    //Set a 32 byte string as your key.
+$config->xuanxuan->key     = '88888888888888888888888888888888'; //Set a 32 byte string as your key.
 
 define('TABLE_IM_CHAT',        '`im_chat`');
 define('TABLE_IM_MESSAGE',     '`im_message`');
