e3421f6d887fe0a8a2a4ebaf75268979f99feb76
Catouse <catouse@gmail.com>
2017-05-04 09:29:42 +0800

    * fix chat view UI.

--- app/views/chat/chat.js
+++ app/views/chat/chat.js
@@ -313,9 +313,11 @@ const ChatPage = React.createClass({
             main: {},
             header: {
               borderBottom: '1px solid ' + Theme.color.border, 
-              padding: '10px 0 10px 50px',
+              padding: '10px 0 10px 48px',
               lineHeight: '28px',
               backgroundColor: Theme.color.pale2,
+              height: 48,
+              boxSizing: 'border-box',
               zIndex: 9
             },
             icon: {
@@ -339,7 +341,7 @@ const ChatPage = React.createClass({
               right: 0
             },
             messageList: {
-              top: 49,
+              top: 48,
             },
             sidebar: {
                 transition: Theme.transition.normal('width', 'opacity', 'visibility')
