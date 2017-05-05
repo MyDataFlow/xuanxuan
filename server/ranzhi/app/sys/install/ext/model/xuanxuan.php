8dd0894aec93b4626801e6ea3e8a23c6672ae223
liugang <liugang@cnezsoft.com>
2017-05-05 10:51:17 +0800

    * Adjust codes.

--- server/ranzhi/app/sys/install/ext/model/xuanxuan.php
+++ server/ranzhi/app/sys/install/ext/model/xuanxuan.php
@@ -1,10 +1,11 @@
-<?
+<?php
 public function createTable($version)
 {
     $result = parent::createTable($version);
     if($result)
     {
-        $this->loadModel('setting')->setItem('system.sys.xuanxuan.global.version', $this->config->xuanxuan->version);
+        $sql = "INSERT INTO {$this->config->db->name}." . TABLE_CONFIG . " (`owner`, `app`, `module`, `section`, `key`, `value`) VALUES ('system', 'sys', 'xuanxuan', 'global', 'version', '{$this->config->xuanxuan->version}')";
+        $this->dbh->query($sql);
     }
     return $result;
 }
