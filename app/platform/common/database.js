1b728a631b8d9234ad0f6ae41c1a998e6a03d50a
Catouse <catouse@gmail.com>
2017-08-29 11:25:54 +0800

    * comment unnessesary database objects.

--- app/platform/common/database.js
+++ app/platform/common/database.js
@@ -17,9 +17,9 @@ class Database {
         }
         this._userIdentify = userIdentify;
         this._db = Dexie.version(DB_VERSION).stores({
-            [Entity.NAME]: Entity.SCHEMA.dexieFormat,
-            [Member.NAME]: Member.SCHEMA.dexieFormat,
-            [Chat.NAME]: Chat.SCHEMA.dexieFormat,
+            // [Entity.NAME]: Entity.SCHEMA.dexieFormat,
+            // [Member.NAME]: Member.SCHEMA.dexieFormat,
+            // [Chat.NAME]: Chat.SCHEMA.dexieFormat,
             [Message.NAME]: Message.SCHEMA.dexieFormat,
         });
     }
@@ -28,13 +28,13 @@ class Database {
         return this._userIdentify;
     }
 
-    get members() {
-        return this._db[Member.NAME];
-    }
+    // get members() {
+    //     return this._db[Member.NAME];
+    // }
 
-    get chats() {
-        return this._db[Chat.NAME];
-    }
+    // get chats() {
+    //     return this._db[Chat.NAME];
+    // }
 
     get chatMessages() {
         return this._db[Message.NAME];
