8f134a53c3e537ddab69069756726ce28e09af64
Catouse <catouse@gmail.com>
2017-09-06 11:02:24 +0800

    * message list support stay in bottom automatic.

--- app/views/chats/message-list.js
+++ app/views/chats/message-list.js
@@ -5,6 +5,36 @@ import MessageListItem from './message-list-item';
 
 class MessageList extends Component {
 
+    scrollToBottom = (smooth) => {
+        this.messageEndEle.scrollIntoView({block: 'end', behavior: smooth ? 'smooth' : 'instant'});
+    }
+
+    componentDidMount() {
+        this.scrollToBottom();
+        this.shouldStayInBottom = true;
+    }
+
+    componentDidUpdate() {
+        if(this.shouldStayInBottom) {
+            this.scrollToBottom();
+        }
+    }
+
+    handleListScrollEvent = e => {
+        const listEle = e.target;
+        this.shouldStayInBottom = (listEle.scrollTop + listEle.offsetHeight) === listEle.scrollHeight;
+    }
+
+    checkHasNewMessages(messages) {
+        if(!this.lastMessage || !messages || !messages.length) {
+            return true;
+        }
+        const thisLastMessage = messages[messages.length - 1];
+        if(thisLastMessage.date > this.lastMessage.date || thisLastMessage.id > this.lastMessage.id) {
+            return true;
+        }
+    }
+
     render() {
         let {
             messages,
@@ -15,17 +45,22 @@ class MessageList extends Component {
         } = this.props;
 
         let lastMessage = null;
+        if(this.checkHasNewMessages(messages)) {
+            this.shouldStayInBottom = true;
+        }
 
         return <div {...other}
             className={HTML.classes('app-message-list', className)}
+            onScroll={this.handleListScrollEvent}
         >
-        {
-            messages && messages.map(message => {
-                const messageListItem = <MessageListItem lastMessage={lastMessage} key={message.gid} message={message}/>;
-                lastMessage = message;
-                return messageListItem;
-            })
-        }
+            {
+                messages && messages.map(message => {
+                    const messageListItem = <MessageListItem lastMessage={lastMessage} key={message.gid} message={message}/>;
+                    lastMessage = message;
+                    return messageListItem;
+                })
+            }
+            <div className="space-sm" ref={e => this.messageEndEle = e}></div>
         </div>;
     }
 }
