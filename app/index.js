39e9048353e3c3036cf23bcd4e8cc612b39c8285
Catouse <catouse@gmail.com>
2017-09-12 15:45:21 +0800

    * support open external url in user default browser.

--- app/index.js
+++ app/index.js
@@ -3,9 +3,26 @@ import './utils/debug';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndexView from './views/index';
+import Platform from 'Platform';
 
 const appElement = document.getElementById('appContainer');
 ReactDOM.render(<IndexView/>, appElement, () => {
     const loadingElement = document.getElementById('loading');
     loadingElement.parentNode.removeChild(loadingElement);
+});
+
+document.body.classList.add(`os-${Platform.env.os}`);
+
+document.addEventListener('click', e => {
+    let target = e.target;
+    while(target && target.tagName !== 'A') {
+        target = target.parentNode;
+    }
+    if(target && target.tagName === 'A' && target.attributes['href']) {
+        const link = target.attributes['href'].value;
+        if(link.startsWith('http://') || link.startsWith('https://')) {
+            Platform.ui.openExternal(link);
+            e.preventDefault();
+        }
+    }
 });
