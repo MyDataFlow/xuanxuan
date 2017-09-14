96669b91bead1861d4032e4bcaf1ae7a7ce8bdb5
Catouse <catouse@gmail.com>
2017-09-14 14:19:50 +0800

    * improve UI of readonly chat.

--- app/views/chats/chat.js
+++ app/views/chats/chat.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import HTML from '../../utils/html-helper';
 import Icon from '../../components/icon';
+import Avatar from '../../components/avatar';
 import Lang from '../../lang';
 import App from '../../core';
 import SplitPane from 'react-split-pane';
@@ -46,18 +47,26 @@ class ChatView extends Component {
         }
 
         const hideSidebar = App.profile.userConfig.isChatSidebarHidden(chat.gid);
+        const isReadOnly = !chat.isCommitter(App.profile.user);
 
         return <div {...other}
             className={HTML.classes('app-chat dock', className, {hidden})}
         >
             <SplitPane className={hideSidebar ? 'soloPane1' : ''} split="vertical" primary="second" maxSize={360} minSize={150} defaultSize={200}>
-                <SplitPane split="horizontal" primary="second" maxSize={500} minSize={80} defaultSize={100}>
-                    <div>
-                        <ChatHeader chat={chat} className="dock-top"/>
-                        <ChatMessages chat={chat} className="dock-bottom"/>
-                    </div>
-                    <ChatSendbox className="dock" chat={chat}/>
-                </SplitPane>
+                {
+                    isReadOnly ? <div className="column single dock">
+                        <ChatHeader chat={chat} className="flex-none"/>
+                        <ChatMessages chat={chat} className="flex-auto relative"/>
+                        <div className="flex-none gray text-gray heading"><Avatar icon="lock-outline"/><div className="title">{Lang.string('chat.committers.blockedTip')}</div></div>
+                    </div> :
+                    <SplitPane split="horizontal" primary="second" maxSize={500} minSize={80} defaultSize={100}>
+                        <div className="column single dock">
+                            <ChatHeader chat={chat} className="flex-none"/>
+                            <ChatMessages chat={chat} className="flex-auto relative"/>
+                        </div>
+                        <ChatSendbox className="dock" chat={chat}/>
+                    </SplitPane>
+                }
                 <ChatSidebar chat={chat}/>
             </SplitPane>
         </div>;
