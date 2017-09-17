43908427164a49c1a5ef5e0cb1c080ac02e9b893
Catouse <catouse@gmail.com>
2017-09-17 10:11:53 +0800

    * support display hd emojicon.

--- app/views/chats/message-content-image.js
+++ app/views/chats/message-content-image.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import HTML from '../../utils/html-helper';
 import App from '../../core';
+import Emojione from '../../components/emojione';
 
 class MessageContentImageView extends Component {
 
@@ -13,13 +14,24 @@ class MessageContentImageView extends Component {
             ...other
         } = this.props;
 
-        const content = message.renderedTextContent(App.im.ui.linkMembersInText);
+        const image = message.imageContent;
+
+        if(image.type === 'emoji') {
+            return <div {...other}
+                className={HTML.classes(' emojione-hd', className)}
+                dangerouslySetInnerHTML={{__html: Emojione.toImage(image.content)}}
+            >
+            {children}
+            </div>;
+        } else if(image.type === 'base64') {
+            return <img src={image.content} />;
+        }
 
         return <div {...other}
             className={HTML.classes('app-message-content-text', className, {
                 'is-content-block': message.isBlockContent
             })}
-            dangerouslySetInnerHTML={{__html: content}}
+            dangerouslySetInnerHTML={{__html: image}}
         >
         {children}
         </div>;
