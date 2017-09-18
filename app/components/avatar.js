5402d1b464cf200cc604676e81151c58d3d904a1
Catouse <catouse@gmail.com>
2017-09-18 08:17:03 +0800

    * add skin props to avatar component.

--- app/components/avatar.js
+++ app/components/avatar.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import HTML from '../utils/html-helper';
 import Icon from './icon';
 
+const getCodeFromString = (str) => {
+    if(!str) {
+        return 0;
+    }
+    return str.split('')
+        .map(char => char.charCodeAt(0))
+        .reduce((current, previous) => previous + current);
+};
+
+const getColorFromCode = (code, other = '70%, 60%') => {
+    return `hsl(${(code * 43) % 360}, ${other})`;
+};
+
 class Avatar extends Component {
 
     render() {
         let {
+            skin,
+            lightSkin,
             image,
             icon,
             label,
@@ -19,13 +34,23 @@ class Avatar extends Component {
             ...other
         } = this.props;
 
+        style = Object.assign({}, style);
         if(size) {
-            style = Object.assign({}, style);
             style.width = HTML.rem(size);
             style.height = style.width;
 
             if(!iconSize) {
                 iconSize = Math.floor(size * 0.5);
+            }
+        }
+
+        if(skin) {
+            if(lightSkin) {
+                style.color = getColorFromCode(getCodeFromString(skin));
+                style.backgroundColor = getColorFromCode(getCodeFromString(skin), '70%, 92%');
+            } else {
+                style.color = '#fff';
+                style.backgroundColor = getColorFromCode(getCodeFromString(skin));
             }
         }
 
--- app/views/common/user-avatar.js
+++ app/views/common/user-avatar.js
@@ -5,25 +5,11 @@ import HTML from '../../utils/html-helper';
 import StatusDot from './status-dot';
 import App from '../../core';
 
-const getCodeFromString = (str) => {
-    if(!str) {
-        return 0;
-    }
-    return str.split('')
-        .map(char => char.charCodeAt(0))
-        .reduce((current, previous) => previous + current);
-};
-
-const getColorFromCode = (code) => {
-    return `hsl(${(code * 43) % 360}, 70%, 60%)`;
-};
-
 class UserAvatar extends Component {
 
     render() {
         let {
             user,
-            style,
             className,
             showStatusDot,
             ...other
@@ -35,24 +21,19 @@ class UserAvatar extends Component {
         }
 
         if(!user) {
-            return <Avatar className={HTML.classes('circle', className)} icon="account" style={style} {...other}>{statusDot}</Avatar>;
+            return <Avatar className={HTML.classes('circle', className)} icon="account" {...other}>{statusDot}</Avatar>;
         }
 
         let avatarImageSrc = user.getAvatar(App.user && App.user.server);
         if(avatarImageSrc) {
             avatarImageSrc = avatarImageSrc + '?' + App.profile.sessionId;
-            return <Avatar className={HTML.classes('circle', className)} image={avatarImageSrc} style={style} {...other}>{statusDot}</Avatar>;
+            return <Avatar className={HTML.classes('circle', className)} image={avatarImageSrc} {...other}>{statusDot}</Avatar>;
         }
         const name = user.realname || user.account;
-        style = Object.assign({
-            color: '#fff',
-            fontWeight: 'lighter',
-            backgroundColor: getColorFromCode(getCodeFromString(name))
-        }, style);
         if(name && name.length) {
-            return <Avatar className={HTML.classes('circle', className)} label={name[0].toUpperCase()} style={style} {...other}>{statusDot}</Avatar>;
+            return <Avatar skin={name} className={HTML.classes('circle', className)} label={name[0].toUpperCase()} {...other}>{statusDot}</Avatar>;
         } else {
-            return <Avatar className={HTML.classes('circle', className)} icon="account" style={style} {...other}>{statusDot}</Avatar>;
+            return <Avatar skin={user.id} className={HTML.classes('circle', className)} icon="account" {...other}>{statusDot}</Avatar>;
         }
     }
 }
