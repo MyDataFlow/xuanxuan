8ccdf051a6a9b3ce1adf320afd6338e4c9f24c8f
Catouse <catouse@gmail.com>
2017-09-21 16:37:44 +0800

    * use skin utils in avatar.

--- app/components/avatar.js
+++ app/components/avatar.js
@@ -1,27 +1,14 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import HTML from '../utils/html-helper';
+import Skin from '../utils/skin';
 import Icon from './icon';
-
-const getCodeFromString = (str) => {
-    if(!str) {
-        return 0;
-    }
-    return str.split('')
-        .map(char => char.charCodeAt(0))
-        .reduce((current, previous) => previous + current);
-};
-
-const getColorFromCode = (code, other = '70%, 60%') => {
-    return `hsl(${(code * 43) % 360}, ${other})`;
-};
 
 class Avatar extends Component {
 
     render() {
         let {
             skin,
-            lightSkin,
             image,
             icon,
             label,
@@ -34,23 +21,13 @@ class Avatar extends Component {
             ...other
         } = this.props;
 
-        style = Object.assign({}, style);
+        style = Object.assign(skin ? Skin.style(skin) : {}, style);
         if(size) {
             style.width = HTML.rem(size);
             style.height = style.width;
 
             if(!iconSize) {
                 iconSize = Math.floor(size * 0.5);
-            }
-        }
-
-        if(skin) {
-            if(lightSkin) {
-                style.color = getColorFromCode(getCodeFromString(skin));
-                style.backgroundColor = getColorFromCode(getCodeFromString(skin), '70%, 92%');
-            } else {
-                style.color = '#fff';
-                style.backgroundColor = getColorFromCode(getCodeFromString(skin));
             }
         }
 
