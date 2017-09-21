f79ee9fd04e09eb12abd0e1dff23121e5aaf88df
Catouse <catouse@gmail.com>
2017-09-21 14:34:50 +0800

    + add hotkey setting dialog.

---
+++ app/views/common/hotkey-setting-dialog.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import Modal from '../../components/modal';
+import HotkeyInputControl from '../../components/hotkey-input-control';
+
+const show = (title, defaultHotkey, onKeySelect, callback) => {
+    let userHotKey = defaultHotkey;
+    return Modal.show({
+        title,
+        onSubmit: () => {
+            if(userHotKey !== defaultHotkey) {
+                onKeySelect && onKeySelect(userHotKey);
+            }
+        },
+        content: <div>
+            <HotkeyInputControl placeholder={defaultHotkey} onChange={key => {
+                userHotKey = key;
+            }}/>
+        </div>
+    }, callback);
+};
+
+export default {
+    show,
+};
