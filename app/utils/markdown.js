86d37cb96996c137f872f9eae37fe5d880faa8b3
Catouse <catouse@gmail.com>
2017-09-22 22:35:57 +0800

    * improve UI of code block in markdown content, support set filename to a code block.

--- app/utils/markdown.js
+++ app/utils/markdown.js
@@ -1,15 +1,37 @@
 import Marked       from 'marked';
 import HighlightJS  from 'highlight.js';
 
+var renderer = new Marked.Renderer();
+
+renderer.code = (code, lang) => {
+    let fileName = null;
+    if(lang) {
+        const colonIndex = lang.indexOf(':');
+        const dotIndex = lang.lastIndexOf('.');
+        if(colonIndex > -1) {
+            fileName = lang.substr(colonIndex + 1);
+            lang = lang.substr(0, colonIndex);
+        } else if(dotIndex > -1) {
+            fileName = lang;
+            lang = lang.substr(dotIndex + 1);
+        }
+    }
+    let result = HighlightJS.highlightAuto(code, lang ? [lang] : undefined);
+    return `<pre${fileName ? (' data-name="' + fileName + '"') : ''}><code data-lang="${lang || (result.language + '?')}" class="lang-${result.language}">${result.value}</code></pre>`;
+};
+
 /**
  * Init markdown helpers
  */
 Marked.setOptions({
-    highlight: (code, lang) => {
-        const result = HighlightJS.highlightAuto(code, lang ? [lang] : undefined);
-        HighlightJS
-        return result.value;
-    },
+    renderer: renderer,
+    // highlight: (code, lang) => {
+    //     let result = HighlightJS.highlightAuto(code, lang ? [lang] : undefined);
+    //     if(lang) {
+
+    //     }
+    //     return result;
+    // },
     gfm: true,
     sanitize: true
 });
