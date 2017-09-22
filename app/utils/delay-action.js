7010d28c501e0ed9bd55aa313c5294f3028516b7
Catouse <catouse@gmail.com>
2017-09-22 16:12:39 +0800

    * add doIm method and isDone attribute to DelayAction class.

--- app/utils/delay-action.js
+++ app/utils/delay-action.js
@@ -4,19 +4,30 @@ class DelayAction {
         this.action = action;
         this.delay = delay;
         this.callback = callback;
+        this.done = true;
     }
 
     do(...params) {
+        this.done = false;
         if(this.actionCallTask) {
             clearTimeout(this.actionCallTask);
         }
         this.actionCallTask = setTimeout(() => {
-            const actionResult = this.action(...params);
-            this.actionCallTask = null;
-            if(typeof this.callback === 'function') {
-                this.callback(actionResult);
-            }
+            this.doIm(...params);
         }, this.delay);
+    }
+
+    doIm(...params) {
+        const actionResult = this.action(...params);
+        this.actionCallTask = null;
+        if(typeof this.callback === 'function') {
+            this.callback(actionResult);
+        }
+        this.done = true;
+    }
+
+    get isDone() {
+        return this.done;
     }
 
     destroy() {
