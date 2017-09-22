408aed57db220713e3b892fe6708ef2f86ff29e3
Catouse <catouse@gmail.com>
2017-09-22 21:39:45 +0800

    * improve code block style in markdown.

--- app/utils/markdown.js
+++ app/utils/markdown.js
@@ -5,8 +5,10 @@ import HighlightJS  from 'highlight.js';
  * Init markdown helpers
  */
 Marked.setOptions({
-    highlight: code => {
-        return HighlightJS.highlightAuto(code).value;
+    highlight: (code, lang) => {
+        const result = HighlightJS.highlightAuto(code, lang ? [lang] : undefined);
+        HighlightJS
+        return result.value;
     },
     gfm: true,
     sanitize: true
