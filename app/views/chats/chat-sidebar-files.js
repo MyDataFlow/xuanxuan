0d055a13d08b9902acc52b0fd2ae2b96972fbb82
Catouse <catouse@gmail.com>
2017-10-11 16:58:24 +0800

    * improve files list UI.

--- app/views/chats/chat-sidebar-files.js
+++ app/views/chats/chat-sidebar-files.js
@@ -6,25 +6,46 @@ import Lang from '../../lang';
 import App from '../../core';
 import FileList from '../common/file-list';
 import Emojione from '../../components/emojione';
+import Spinner from '../../components/spinner';
 
 class ChatSidebarFiles extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            files: []
+            files: [],
+            loading: true
         };
     }
 
     loadFiles() {
         const chat = this.props.chat;
         App.im.chats.getChatFiles(chat).then(files => {
-            this.setState({files});
+            this.setState({files, loading: false});
         });
     }
 
     componentDidMount() {
         this.loadFiles();
+    }
+
+    renderLoading() {
+        return <div className="dock center-content" style={{top: HTML.rem(50)}}>
+            <Spinner label={Lang.string('chat.sidebar.tab.files.loading')}/>
+        </div>;
+    }
+
+    renderEmptyFileList() {
+        return <div className="dock center-content" style={{top: HTML.rem(50)}}>
+            <div>
+                <div className="text-center" dangerouslySetInnerHTML={{__html: Emojione.toImage(':blowfish:')}}></div>
+                <div className="text-gray small">{Lang.string('chat.sidebar.tab.files.noFilesHere')}</div>
+            </div>
+        </div>;
+    }
+
+    renderFileList(files) {
+        return <FileList listItemProps={{smallIcon: true, showSender: true}} className="white rounded" files={files}/>;
     }
 
     render() {
@@ -35,18 +56,13 @@ class ChatSidebarFiles extends Component {
             ...other
         } = this.props;
 
-        const {files} = this.state;
+        const {files, loading} = this.state;
 
         return <div {...other}
             className={HTML.classes('app-chat-sidebar-files has-padding', className)}
         >
             {
-                files.length ? <FileList listItemProps={{smallIcon: true, showSender: true}} className="white rounded" files={files}/> : <div className="dock center-content" style={{top: HTML.rem(50)}}>
-                    <div>
-                        <div className="text-center" dangerouslySetInnerHTML={{__html: Emojione.toImage(':blowfish:')}}></div>
-                        <div className="text-gray small">{Lang.string('chat.sidebar.tab.files.noFilesHere')}</div>
-                    </div>
-                </div>
+                loading ? this.renderLoading() : files.length ? this.renderFileList(files) : this.renderEmptyFileList()
             }
             {children}
         </div>;
