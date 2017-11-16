d9e923bb3bd2cf4d7a11606a9880613c5f56182e
Catouse <catouse@gmail.com>
2017-11-16 13:49:55 +0800

    * add node modules to external api.

--- app/exts/external-api.js
+++ app/exts/external-api.js
@@ -1,9 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReactSplitPane from 'react-split-pane';
+import EmojionePicker from 'emojione-picker';
+import marked from 'marked';
+import md5 from 'md5';
+import fse from 'fs-extra';
+import extractZip from 'extract-zip';
+import emojione from 'emojione';
+import DraftJs from 'draft-js';
+import compareVersions from 'compare-versions';
+import hotkeys from 'hotkeys-js';
+import pinyin from 'pinyin';
+import uuid from 'uuid';
 import platform from 'Platform';
 import components from '../components';
 import lang from '../lang';
 import utils from '../utils';
 import app from '../core';
 import views from '../views/external';
+
+const nodeModules = {
+    React,
+    ReactDOM,
+    ReactSplitPane,
+    EmojionePicker,
+    marked,
+    md5,
+    fse,
+    extractZip,
+    emojione,
+    DraftJs,
+    compareVersions,
+    hotkeys,
+    pinyin,
+    uuid,
+};
 
 export default {
     lang,
@@ -12,4 +43,5 @@ export default {
     platform,
     app,
     views,
+    nodeModules
 };
