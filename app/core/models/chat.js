05fed7fd7546e8aee052f44790a1377c3c67a7bc
Catouse <catouse@gmail.com>
2017-12-07 17:43:20 +0800

    * fix message order not work sometimes.

--- app/core/models/chat.js
+++ app/core/models/chat.js
@@ -583,7 +583,6 @@ class Chat extends Entity {
         }
 
         let noticeCount = this.noticeCount;
-        let newMessageCount = 0;
         let lastActiveTime = this.lastActiveTime;
         messages.forEach(message => {
             if (message.date) {
@@ -592,7 +591,6 @@ class Chat extends Entity {
                     checkMessage.reset(message);
                 } else {
                     this._messages.push(message);
-                    newMessageCount += 1;
                     if (!localMessage && userId !== message.senderId) {
                         message.unread = true;
                         noticeCount += 1;
@@ -613,9 +611,7 @@ class Chat extends Entity {
         this.lastActiveTime = lastActiveTime;
         this.noticeCount = noticeCount;
 
-        if (newMessageCount) {
-            this._messages = ChatMessage.sort(this._messages);
-        }
+        this._messages = ChatMessage.sort(this._messages);
 
         if (limitSize && this._messages.length > MAX_MESSAGE_COUNT) {
             this._messages.splice(0, this._messages.length - MAX_MESSAGE_COUNT);
