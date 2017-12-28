9d968190918de1f9a5c3dfd139d530185d8e044e
liugang <liugang@cnezsoft.com>
2017-12-28 15:02:29 +0800

    * Format codes. * Fix bug.

--- server/ranzhi/app/sys/install/ext/model/xuanxuan.php
+++ server/ranzhi/app/sys/install/ext/model/xuanxuan.php
@@ -4,7 +4,7 @@ public function createTable($version)
     $result = parent::createTable($version);
     if($result)
     {
-        $sql = "INSERT INTO {$this->config->db->name}." . TABLE_CONFIG . " (`owner`, `app`, `module`, `section`, `key`, `value`) VALUES ('system', 'sys', 'xuanxuan', 'global', 'version', '{$this->config->xuanxuan->version}')";
+        $sql = "INSERT INTO `{$this->config->db->name}`.`{$this->config->db->prefix}sys_config` (`owner`, `app`, `module`, `section`, `key`, `value`) VALUES ('system', 'sys', 'xuanxuan', 'global', 'version', '{$this->config->xuanxuan->version}')";
         $this->dbh->query($sql);
     }
     return $result;
