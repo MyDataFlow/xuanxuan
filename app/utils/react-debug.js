fce3edb7c52c3bc9745f913620d5609ddab90222
Catouse <catouse@gmail.com>
2018-03-23 14:57:13 +0800

    * add why-did-update module to improve perfermance.

---
+++ app/utils/react-debug.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+if (process.env.NODE_ENV !== 'production') {
+    const {whyDidYouUpdate} = __non_webpack_require__('why-did-you-update'); // eslint-disable-line
+    whyDidYouUpdate(React);
+}
