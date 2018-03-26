d5a6592a09c5fcd42f689e806525830cb303afe5
Catouse <catouse@gmail.com>
2018-03-26 10:33:25 +0800

    * add try/catch to import debug module.

--- app/utils/react-debug.js
+++ app/utils/react-debug.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
 if (process.env.NODE_ENV !== 'production') {
-    const {whyDidYouUpdate} = __non_webpack_require__('why-did-you-update'); // eslint-disable-line
-    whyDidYouUpdate(React);
+    try {
+        const {whyDidYouUpdate} = __non_webpack_require__('why-did-you-update'); // eslint-disable-line
+        whyDidYouUpdate(React);
+    } catch (error) {
+        console.warn('Cannot find the debug module why-did-you-update.');
+    }
 }
