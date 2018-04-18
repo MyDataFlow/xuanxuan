e41612ae71c43fd2841b30f6411be84f0d773818
Catouse <catouse@gmail.com>
2018-04-18 17:04:46 +0800

    * remove debug code.

--- xxc/app/core/models/notification-message.js
+++ xxc/app/core/models/notification-message.js
@@ -58,15 +58,6 @@ export default class NotificationMessage extends ChatMessage {
             data = data.data;
         }
 
-        if (DEBUG) {
-            if (!data.subtitle) {
-                data.subtitle = '副标题';
-            }
-            if (!data.content) {
-                data.content = 'JavaScript ( JS ) 是一种具有头等函数的轻量级解释型或即时编译型的编程语言。虽然它是作为开发web 页面的脚本语言而出名的，但是它也被用到了很多非浏览器环境中，例如 node.js、 Apache CouchDB 和 Adobe Acrobat。JavaScript是一种基于原型编程、多范式的动态脚本语言，并且支持面向对象、命令式和声明式（如函数式编程）风格。了解更多 JavaScript。';
-            }
-        }
-
         let content = `#### ${data.title}`;
         if (stringHelper.isNotEmpty(data.subtitle)) {
             content += `\n##### ${data.subtitle}`;
