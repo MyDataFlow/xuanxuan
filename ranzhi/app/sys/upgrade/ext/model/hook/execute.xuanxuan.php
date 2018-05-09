14ef2c28a7497d7a196b775c944d95ef0c7b6621
liugang <liugang@cnezsoft.com>
2018-05-09 15:40:36 +0800

    * Fix bug that upgrade from a lower version it occurs an error.

--- ranzhi/app/sys/upgrade/ext/model/hook/execute.xuanxuan.php
+++ ranzhi/app/sys/upgrade/ext/model/hook/execute.xuanxuan.php
@@ -1,5 +1,5 @@
 <?php
-if($fromVersion < '4_1')
+if((strpos($fromVersion, 'pro') === false && $fromVersion < '4_1') or (strpos($fromVersion, 'pro') !== false && $fromVersion < 'pro2_1'))
 {
     $this->execSQL($this->app->getBasepath() . 'db' . DS . 'xuanxuan.sql');
 }
