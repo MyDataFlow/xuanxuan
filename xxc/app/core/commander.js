6fea670c0ccf3e63d4220936e582400ef0756882
Catouse <catouse@gmail.com>
2018-06-14 11:15:29 +0800

    * refactor command and support more params.

--- xxc/app/core/commander.js
+++ xxc/app/core/commander.js
@@ -1,8 +1,30 @@
+import {getSearchParam} from '../utils/html-helper';
 
-
+/**
+ * 用于保存命令的公共上下文参数数据
+ * (Save current shared context)
+ *
+ * @ignore
+ * @private
+ */
 const context = {};
+
+/**
+ * 用于保存注册的命令
+ * (Save registered commands)
+ *
+ * @ignore
+ * @private
+ */
 const commands = {};
 
+/**
+ * 设置当前命令上下文参数
+ * (Set Command context data)
+ *
+ * @param {any} data 上下文参数 (Command context data)
+ * @return {void}
+ */
 export const setCommandContext = (data) => {
     if (data) {
         if (typeof data !== 'object') {
@@ -12,14 +34,44 @@ export const setCommandContext = (data) => {
     }
 };
 
-export const getCommandContext = () => {
-    return Object.assign({}, context);
+/**
+ * 获取当前命令上下文参数
+ * (Get current command context data)
+ *
+ * @param {?object} [newContext=null] 新的上下文参数 (New command context)
+ * @return {object}
+ */
+export const getCommandContext = (newContext) => {
+    return Object.assign({}, context, newContext);
 };
 
+/**
+ * 执行命令
+ * (Execute command)
+ *
+ * @param {string} commandName 命令名称 (Command name)
+ * @param {...string} params 命令参数 (Command params)
+ * @return {Promise<any, Error>} 通过 Promise 返回命令执行结果 (Return result with Promise)
+ */
 export const execute = (commandName, ...params) => {
     const command = commands[commandName];
     if (command) {
-        const commandContext = getCommandContext();
+        let searchOptions = null;
+        if (params && params.length && params[params.length - 1][0] === '?') {
+            searchOptions = getSearchParam(params[params.length - 1]);
+        }
+        const commandContext = getCommandContext(searchOptions ? {options: searchOptions} : null);
+        if (command.context) {
+            const typeOfCommandContext = typeof command.context;
+            if (typeOfCommandContext === 'function') {
+                Object.assign(commandContext, command.context(commandContext, ...params));
+            } else if (typeOfCommandContext === 'object') {
+                Object.assign(commandContext, command.context);
+            } else {
+                Object.assign(commandContext, {data: command.context});
+            }
+        }
+
         const result = command.func(commandContext, ...params);
 
         if (DEBUG) {
@@ -38,25 +90,63 @@ export const execute = (commandName, ...params) => {
         }
         return Promise.resolve(result);
     }
-    return Promise.reject(`Unknown command '${command.name}'.`);
+    return Promise.reject(new Error(`Unknown command '${command.name}'.`));
 };
 
-export const executeCommand = (commandText, commandContext) => {
+/**
+ * 根据命令文本字符串执行命令
+ * (Execute command from command text string)
+ *
+ * @param {string} commandText 命令文本字符串 (Command text string)
+ * @param {object} [commandContext=null] 命令上下文参数 (Command context data)
+ * @return {Promise<any, Error>} 通过 Promise 返回命令执行结果 (Return result with Promise)
+ */
+export const executeCommand = (commandText, commandContext = null) => {
     console.log('executeCommand', commandText);
     setCommandContext(commandContext);
     return execute(...commandText.split('/'));
 };
 
-export const registerCommand = (name, func) => {
+/**
+ * 注册命令
+ * (Register a command)
+ *
+ * @param {string|object} name 命令名称或者命令配置对象 (Command name or command config object)
+ * @param {?function(context: object, params: ...string)} [func=null] 命令操作函数 (Command function)
+ * @param {?object|?function(context: object, params: ...string} [commandContext=null] 命令上下文参数 (Command context data)
+ * @return {boolean} 如果为 true，则命令注册成功；否则注册失败，通常失败的原因是已有相同名称的命令注册过 (If return true, then register success, else fail)
+ */
+export const registerCommand = (name, func = null, commandContext = null) => {
     const command = typeof name === 'object' ? Object.assign({}, name) : {name};
     if (typeof func === 'function') {
         command.func = func;
     }
+    if (commandContext) {
+        command.context = commandContext;
+    }
+    if (commands[command.name]) {
+        if (DEBUG) {
+            console.wran(`Command register failed, because the command '${command.name}' is already registered.`);
+        }
+        return false;
+    }
     commands[command.name] = command;
+    return true;
 };
 
+/**
+ * 取消注册命令
+ * (Unregister command)
+ *
+ * @param {string} name 命令名称 (Command name)
+ * @return {booean} 如果为 true，表示成功取消注册命令；否则取消注册失败，通常失败的原因是该名称的命令从没有注册过，或者已经被取消 (If return true, then unregister success，else fail)
+ */
 export const unregisterCommand = name => {
-    delete commands[name];
+    if (commands[name]) {
+        delete commands[name];
+        return true;
+    }
+    return false;
 };
 
 export default {
