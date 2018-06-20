02f1aab70dcfe31d1bae2a52a7f158ee577eefa7
滔哥 <admin@lvtao.net>
2018-06-20 08:33:49 +0800

    * Change the field name.

--- xxb/backend/db/upgradexxb1.2.sql
+++ xxb/backend/db/upgradexxb1.2.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `sys_entry` ADD `status` ENUM('online','offline')  NOT NULL  DEFAULT 'online'  AFTER `category`;
 ALTER TABLE `sys_entry` ADD `version` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `buildin`;
-ALTER TABLE `sys_entry` ADD `target` VARCHAR(255) NOT NULL DEFAULT 'ranzhi' AFTER `version`;
-ALTER TABLE `sys_entry` ADD `package` INT(11)  NOT NULL DEFAULT '0'  AFTER `target`;
+ALTER TABLE `sys_entry` ADD `platform` VARCHAR(255) NOT NULL DEFAULT 'ranzhi' AFTER `version`;
+ALTER TABLE `sys_entry` ADD `package` INT(11)  NOT NULL DEFAULT '0'  AFTER `platform`;
