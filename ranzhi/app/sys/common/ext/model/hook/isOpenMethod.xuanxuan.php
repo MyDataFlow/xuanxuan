b906d0c6379033ad2d1e929c2083b1de0895c09a
滔哥 <admin@lvtao.net>
2018-07-02 10:53:30 +0800

    * Fix bug.

--- ranzhi/app/sys/common/ext/model/hook/isOpenMethod.xuanxuan.php
+++ ranzhi/app/sys/common/ext/model/hook/isOpenMethod.xuanxuan.php
@@ -1,2 +1,4 @@
 <?php
-if($module == 'entry' and $method == 'auth') return true;
+if($module == 'attach' and $method == 'upload')   return true;
+if($module == 'attach' and $method == 'download') return true;
+if($module == 'entry'  and $method == 'auth') return true;
