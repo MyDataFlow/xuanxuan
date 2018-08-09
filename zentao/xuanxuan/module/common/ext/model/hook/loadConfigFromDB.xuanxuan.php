0f98030d3245a4706c95575101aada018dbc15bb
liugang <liugang@cnezsoft.com>
2018-08-09 13:49:28 +0800

    * Set default server.

--- zentao/xuanxuan/module/common/ext/model/hook/loadConfigFromDB.xuanxuan.php
+++ zentao/xuanxuan/module/common/ext/model/hook/loadConfigFromDB.xuanxuan.php
@@ -1,3 +1,3 @@
 <?php
 $sn = $this->loadModel('setting')->getItem('owner=system&module=xuanxuan&key=key');
-if(empty($sn)) $this->loadModel('setting')->setItem('system.xuanxuan..key', $this->loadModel('setting')->computeSN());
+if(empty($sn)) $this->setting->setItem('system.xuanxuan..key', $this->setting->computeSN());
