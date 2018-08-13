59fc6784bf76ac399a2c8b135b008b570354495a
滔哥 <admin@lvtao.net>
2018-08-13 10:01:26 +0800

    * Fix bug.

--- xxd/build_xgo_platform.sh
+++ xxd/build_xgo_platform.sh
@@ -10,29 +10,32 @@ if [ ! -d $folder ]; then
     mkdir $folder
 fi
 xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=darwin/amd64 .
-cp -ra config $folder
+mv $folder/xxd* $folder/xxd
+cp -Ra config $folder
 tar zcf $folder.tar.gz $folder
 rm -rf $folder
 
 
-folder=$version.linux-x64
+folder=$version.linux.x64
 if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build linux x64 ..."
 xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=linux/amd64 .
-cp -ra config $folder
+mv $folder/xxd* $folder/xxd
+cp -Ra config $folder
 tar zcf $folder.tar.gz $folder
 rm -rf $folder
 
 
-folder=$version.linux-ia32
+folder=$version.linux.ia32
 if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build linux ia32 ..."
 xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=linux/386 .
-cp -ra config $folder
+mv $folder/xxd* $folder/xxd
+cp -Ra config $folder
 tar zcf $folder.tar.gz $folder
 rm -rf $folder
 
@@ -42,8 +45,9 @@ if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build win64 ..."
-xgo -out $folder/xxd.exe --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/amd64 .
-cp -ra config $folder
+xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/amd64 .
+mv $folder/xxd* $folder/xxd.exe
+cp -Ra config $folder
 zip -rq $folder.zip $folder
 rm -rf $folder
 
@@ -53,11 +57,11 @@ if [ ! -d $folder ]; then
     mkdir $folder
 fi
 echo "build win32 ..."
-xgo -out $folder/xxd.exe --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/386 .
+xgo -out $folder/xxd --deps=https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2 --targets=windows/386 .
+mv $folder/xxd* $folder/xxd.exe
 cp -ra config $folder
 zip -rq $folder.zip $folder
 rm -rf $folder
-
 
 echo "build end "
 
