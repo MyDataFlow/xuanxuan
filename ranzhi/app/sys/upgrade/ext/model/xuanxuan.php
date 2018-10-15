b9ba22a6ca964f8c8a8aa5c8f6f48d6e8479b2be
liugang <liugang@cnezsoft.com>
2018-10-15 11:09:14 +0800

    * Add default entry.

--- ranzhi/app/sys/upgrade/ext/model/xuanxuan.php
+++ ranzhi/app/sys/upgrade/ext/model/xuanxuan.php
@@ -30,6 +30,7 @@ public function upgradeXuanxuan($fromVersion)
         case '1.5.0' :
         case '1.6.0' : $this->execSQL($this->getUpgradeFile('xuanxuan1.6.0'));
         case '2.0.0' : $this->execSQL($this->getUpgradeFile('xuanxuan2.0.0'));
+            $this->installSSOEntry();
         default : $this->loadModel('setting')->setItem('system.sys.xuanxuan.global.version', $this->config->xuanxuan->version);
     }
 }
@@ -61,6 +62,58 @@ public function processMessageStatus()
             $userMessages[$user][$message->gid] = $message->gid;
         }
     }
+
+    return !dao::isError();
+}
+
+/**
+ * Install sso entry.
+ *
+ * @access public
+ * @return bool
+ */
+public function installSSOEntry()
+{
+    $file = new stdclass();
+    $file->pathname    = '201810/f_8db2fa542a1e087d63d45d8bc1185361.zip';
+    $file->title       = 'sso';
+    $file->extension   = 'zip';
+    $file->size        = 89674;
+    $file->objectType  = 'entry';
+    $file->objectID    = 0;
+    $file->createdBy   = $this->app->user->account;
+    $file->createdDate = helper::now();
+    $file->public      = 1;
+    $this->dao->insert(TABLE_FILE)->data($file)->exec();
+
+    if(dao::isError()) return false;
+
+    $fileID = $this->dao->lastInsertId();
+
+    $entry = new stdclass();
+    $entry->name        = 'sso';
+    $entry->abbr        = 'sso';
+    $entry->code        = 'sso';
+    $entry->buildin     = 1;
+    $entry->version     = '1.0.0';
+    $entry->platform    = 'xuanxuan';
+    $entry->package     = $fileID;
+    $entry->integration = 1;
+    $entry->open        = 'iframe';
+    $entry->key         = '7a171c33d02d172fc0f1cf4cb93edfd6';
+    $entry->ip          = '*';
+    $entry->logo        = '';
+    $entry->login       = 'http://xuan.im';
+    $entry->control     = 'none';
+    $entry->size        = 'max';
+    $entry->position    = 'default';
+    $entry->sso         = 1;
+
+    $this->dao->insert(TABLE_ENTRY)->data($entry)->exec();
+
+    $entryID = $this->dao->lastInsertId();
+
+    $this->dao->update(TABLE_FILE)->set('objectID')->eq($entryID)->where('id')->eq($fileID)->exec();
 
     return !dao::isError();
 }
