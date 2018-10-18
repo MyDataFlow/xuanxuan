dc5051368d96baa86941846d1b22e4d3066d5174
Catouse <catouse@gmail.com>
2018-10-18 14:34:56 +0800

    * update zentao extension.

--- xxc/examples/extensions/zentao/index.js
+++ xxc/examples/extensions/zentao/index.js
@@ -17,30 +17,37 @@ let extension = null;
 module.exports = {
     onAttach: ext => {
         extension = ext;
-        if (extension.entryUrl) {
-            extension.serverEntryHost = new URL(extension.entryUrl).host;
-        }
     },
     // onDetach: ext => {
     // },
     urlInspectors: [{
+        // test 函数用于判断一个url是否要进行特殊卡片渲染
         test: url => {
-            const urlHost = new URL(url).host;
-            return [extension.serverEntryHost, '.5upm.com', 'pms.zentao.net', 'demo.zentao.net', 'pro.demo.zentao.net', '.zentaopm.com', 'test.zentao.net'].some(x => urlHost.endsWith(x));
+            const urlObj = new URL(url)
+            const urlHost = urlObj.host;
+            return ['.5upm.com', 'pms.zentao.net', 'demo.zentao.net', 'pro.demo.zentao.net', '.zentaopm.com', 'test.zentao.net'].some(x => urlHost.endsWith(x));
         },
+        // getURL 用于转换实际使用的地址
         getUrl: url => {
+            // 在这里即可以直接使用 extension.getEntryUrl(url) 走 entry/visit， 也可以自己进行其他处理（如果不需要验证，直接返回 url）。
             return extension.getEntryUrl(url);
         },
+        // noMeta=true 用于忽略系统内置的方式
         noMeta: true,
+        // 用于决定最终url渲染的卡片配置
         inspect: (url) => {
             const cardMeta = {};
             cardMeta.title = null;
             cardMeta.webviewContent = true;
             cardMeta.icon = false;
             cardMeta.content = {
+                // insertCSS 用于将 css 注入到 iframe 页面内，用于改变源页面外观
                 insertCss: injectCss,
+                // url iframe 地址
                 src: url,
+                // iframe 高度和宽度设置
                 style: {height: '400px', width: '550px'},
+                // type 为 iframe 和 webview 方式则直接在卡片内加载页面
                 type: isWindows7 ? 'iframe' : 'webview'
             };
             return cardMeta;
